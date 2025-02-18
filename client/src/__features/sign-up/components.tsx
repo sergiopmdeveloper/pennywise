eed35cd8dc1e2b4298f6a514f8019653412bb740
Sergio <sergio.pm.developer@gmail.com>
2025-02-18 00:36:40 +0000

    feat(client): validate fields in the sign up form

--- client/src/__features/sign-up/components.tsx
+++ client/src/__features/sign-up/components.tsx
@@ -1,3 +1,4 @@
+import { signUpSchema, type SignUpData } from '@/__features/sign-up/schemas';
 import { Button } from '@/__ui/components/ui/button';
 import {
   Card,
@@ -7,14 +8,40 @@ import {
   CardHeader,
   CardTitle,
 } from '@/__ui/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/__ui/components/ui/form';
 import { Input } from '@/__ui/components/ui/input';
-import { Label } from '@/__ui/components/ui/label';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
 
 /**
  * Sign up form component.
  */
 export function SignUpForm() {
+  const form = useForm<SignUpData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  /**
+   * Handles the sign up form submission.
+   * @param {SignUpData} data - The new user data.
+   */
+  function onSubmit(data: SignUpData) {
+    console.log(data);
+  }
+
   return (
     <Card className="w-sm">
       <CardHeader>
@@ -23,28 +50,62 @@ export function SignUpForm() {
       </CardHeader>
 
       <CardContent>
-        <form className="space-y-6" id="sign-up-form">
-          <div className="grid gap-2">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" autoComplete="name" placeholder="Pennywise" />
-          </div>
+        <Form {...form}>
+          <form className="space-y-6" id="sign-up-form" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
 
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" autoComplete="email" placeholder="pennywise@gmail.com" />
-          </div>
+                  <FormControl>
+                    <Input autoComplete="name" placeholder="Pennywise" {...field} />
+                  </FormControl>
 
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-
-            <Input
-              id="password"
-              type="password"
-              autoComplete="current-password"
-              placeholder="*********"
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </form>
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+
+                  <FormControl>
+                    <Input autoComplete="email" placeholder="pennywise@gmail.com" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      type="password"
+                      autoComplete="current-password"
+                      placeholder="*********"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
 
         <p className="mt-6 text-sm">
           Do you already have an account?{' '}
---
+++ client/src/__features/sign-up/schemas.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const signUpSchema = z.object({
+  name: z
+    .string()
+    .regex(/^[A-Za-z][A-Za-z\s'-]*[A-Za-z]$|^$/, 'Name format is invalid.')
+    .optional(),
+  email: z.string().min(1, 'Email field is required.').email('Email format is invalid.'),
+  password: z
+    .string()
+    .min(1, 'Password field is required.')
+    .min(6, 'Password must be at least 6 characters.'),
+});
+
+export type SignUpData = z.infer<typeof signUpSchema>;
